0d1dbe062787bd0bd0f5c63559f787866c306955
loub <lyoussar@gmail.com>
2022-04-20 07:33:55 +0200

    nls2_changes

--- R/nls2_fit.r
+++ R/nls2_fit.r
@@ -48,8 +48,8 @@
 nls2_fit <-
   function(inp,
            cores = 1,
-           decay = seq(.08, 0.11, 0.02),
-           delay = seq(0, 10, 0.1),
+           decay = seq(.01, 1, by = .02),
+           delay = seq(0, 10, by = 0.1),
            k = seq(0.1, 1, 0.2),
            bg = 0.2) {
     #order the input
@@ -82,13 +82,14 @@ nls2_fit <-
     #boarders
     # upper_STD <- list(decay = log(2)/(1/60), delay = max(time),
     #                   k = 1/(log(2)/(60)))
-    lower_STD <- list(decay = 0.01, delay = 0.001)
+    lower_STD <- lower_STD <- list(decay = log(2)/(60), delay = 0.001, 
+                                   k = 0.01, bg = 0)
     # upper_ABG <- list(decay = log(2)/(1/60), delay = max(time),
     #                   k = 1/(log(2)/(60)))
-    lower_ABG <- list(decay = 0.01, delay = 0.001)
+    lower_ABG <- list(decay = log(2)/(60), delay = 0.001, k= 0.01)
     #models
     model_STD <- inty ~ I(time < delay) * I(k / decay) + 
-      (time >= delay) * I(bg + (k / decay - bg) * (exp(-decay * (time - delay))))
+      (time >= delay) * I(bg + (k / decay) * (exp(-decay * (time - delay))))
 
     model_ABG <- inty ~ I(time < delay) * I(k / decay) + 
       (time >= delay) * I(k / decay * (exp(-decay * (time - delay))))
